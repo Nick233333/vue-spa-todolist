855506ccbe965c12a1faad86eed4c9b4fa940015
nick <nick_php@163.com>
2018-07-24 18:10:10 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,11 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
