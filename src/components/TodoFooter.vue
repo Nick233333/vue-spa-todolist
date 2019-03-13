06bc06b6458cdd154cfac8face39b4702db0e77c
nick <nick_php@163.com>
2019-03-13 10:34:25 +0800

    :pencil2: rename

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="d-flex justify-content-between">
+        <span class="float-left">已完成 {{ completed }}</span>
+        <button class="btn btn-danger btn-sm w-25" @click="clear()">清空</button>
+        <span class="float-right">未完成 {{ uncompleted }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoFooter',
+    props: ['todos'],
+    computed: {
+        uncompleted() {
+            return this.todos.filter((todo) => {
+                return todo.completed === false;
+            }).length;
+        },
+        completed() {
+            return this.todos.filter((todo) => {
+                return todo.completed === true;
+            }).length;
+        }
+    },
+    methods: {
+        clear() {
+            localStorage.removeItem('vue-todolist');
+            this.$emit('delete');
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <h3>My todolist<span class="badge badge-secondary float-right">{{ count }}</span></h3>
+</template>
+
+<script>
+export default {
+    name: 'TodoHeader',
+    props: ['todos'],
+    computed: {
+        count() {
+            if (this.todos.length === 0) {
+              return;
+        }
+            return this.todos.length;
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
